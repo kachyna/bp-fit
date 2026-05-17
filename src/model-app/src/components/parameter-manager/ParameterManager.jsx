e77e9ad4cb741e60d4fec15b9be0e9cabe98ca0d
René Kachyňa <kachynarene@gmail.com>
2026-05-17 17:49:31 +0200

    feat: add scenario inputs to parameter manager

--- src/model-app/src/components/parameter-manager/ParameterManager.jsx
+++ src/model-app/src/components/parameter-manager/ParameterManager.jsx
@@ -1,57 +1,34 @@
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from "@/components/ui/collapsible"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { ChevronDown, InfoIcon } from "lucide-react"
+
+import { InfoIcon } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 import { useModelStore } from "@/store/useModelStore"
+import { SCENARIO_KEYS } from "@/constants/config"
 import { PARAM_DETAILS } from '@/constants/param-details'
 import { PARAM_GROUPS } from '@/constants/param-groups'
-import { Card, CardContent } from "@/components/ui/card"
-import { CommonParameterInput } from "./common-parameter"
 import { CategoryDropdown } from "@/components/parameter-manager/parameter-category-dropown"
+import { CommonParameterInput } from "./common-parameter"
+import { ScenarioParameterInput } from "./scenario-parameter"
 
-const getScenarioParamValues = (scenario, paramKey) => {
-    const { params } = useModelStore()
-
-    const ret = {}
-
-    Object.keys(params.SCENARIOS).forEach(scenario => {
-        ret[scenario] = params.SCENARIOS[scenario][paramKey]
-    })
-    return ret
-}
-
-const getCommonParamInputHtml = (paramKey, paramLabel, paramDescription) => {
-    return (<div className="space-y-1.5">
-        <Label htmlFor={`common-${paramKey}`} className="text-xs text-slate-500">{paramLabel}</Label>
-        <Input
-            id={`common-${paramKey}`}
-            type="number"
-            step="0.1"
-            value={params.COMMON_PARAMS[paramKey]}
-            onChange={(e) => updateParameter("COMMON_PARAMS", paramKey, e.target.value)}
-            placeholder="1.5"
-            className="h-8 text-sm"
-        />
-    </div>
-    )
-}
 
 export function ParameterManager() {
 
     const params = useModelStore((state) => state.params);
     const updateParameter = useModelStore((state) => state.updateParameter);
+
+    const scenarioParamKeys = new Set()
+
+    SCENARIO_KEYS.forEach(scenario => {
+        const scenarioParams = params.SCENARIOS[scenario] || {}
+        Object.keys(scenarioParams).forEach(paramKey => {
+            const value = scenarioParams[paramKey]
+
+            // Scenario and datacenter type specific parameters will be in another category, so we want to filter them out
+            if (typeof value === "number") {
+                scenarioParamKeys.add(paramKey)
+            }
+        })
+    })
 
     const renderCommonParams = {
         paramLabel: b,
@@ -95,7 +72,7 @@ export function ParameterManager() {
             </CategoryDropdown>
 
             <CategoryDropdown categoryName="Scénářové parametry">
-                <Alert>
+                <Alert className="mb-3 shadow-sm border-slate-200">
                     <InfoIcon />
                     <AlertTitle className="text-sm">Scénářové parametry</AlertTitle>
                     <AlertDescription>
@@ -104,63 +81,33 @@ export function ParameterManager() {
                         <span className="text-xs font-semibold">PESIMISTICKÝ - REALISTICKÝ - OPTIMISTICKÝ.</span>
                     </AlertDescription>
                 </Alert>
+
+                <div className="space-y-3">
+                    {Array.from(scenarioParamKeys).map(paramKey => {
+                        
+                        const paramDetails = PARAM_DETAILS[paramKey] || {}
+                        const paramLabel = paramDetails.label || paramKey
+                        const paramDescription = paramDetails.description || ""
+                        const values = Object.fromEntries(
+                            SCENARIO_KEYS.map(scenario => [
+                                scenario,
+                                params.SCENARIOS[scenario][paramKey]
+                            ])
+                        );
+
+                        return (
+                            <ScenarioParameterInput
+                                key={paramKey}
+                                paramKey={paramKey}
+                                label={paramLabel}
+                                description={paramDescription}
+                                paramValues={values}
+                                onChange={(value, scenario) => updateParameter(scenario, paramKey, Number(value))}
+                            />
+                        )
+                    })}
+                </div>
             </CategoryDropdown>
-
-            <Collapsible defaultOpen className="group/collapsible">
-                <CollapsibleTrigger>
-                    <div className="flex flex-row items-center space-around text-sm font-medium text-slate-900 group-data-[state=open]/collapsible:text-slate-600 group-data-[state=open]/collapsible:mb-4">
-                        <span className="font-semibold text-xs uppercase">Common parameters</span>
-                        <ChevronDown className="ml-auto size-5 transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                    </div>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-
-
-                    <Card>
-                        <CardContent>
-
-                            <div className="space-y-1.5">
-                                <Label className="text-xs text-slate-500">Scenarovy parametr</Label>
-                                <div className="flex flex-row gap-2">
-                                    <Input className="h-8 text-sm" />
-                                    <Input className="h-8 text-sm" />
-                                    <Input className="h-8 text-sm" />
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                    <Card>
-                        <CardContent>
-                            <div className="space-y-1.5">
-                                <Label className="text-xs text-slate-500">Scenarovy parametr</Label>
-                                <div className="flex flex-row gap-2">
-                                    <div>
-                                        <div>
-                                            <Label className="text-xs text-slate-500">Pesimisticky</Label>
-                                            <Input className="h-8 text-sm" />
-                                            <Input className="h-8 text-sm" />
-                                            <Input className="h-8 text-sm" />
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <Label className="text-xs text-slate-500">Realisticky</Label>
-                                        <Input className="h-8 text-sm" />
-                                        <Input className="h-8 text-sm" />
-                                        <Input className="h-8 text-sm" />
-                                    </div>
-                                    <div>
-                                        <Label className="text-xs text-slate-500">Optimisticky</Label>
-                                        <Input className="h-8 text-sm" />
-                                        <Input className="h-8 text-sm" />
-                                        <Input className="h-8 text-sm" />
-                                    </div>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </CollapsibleContent>
-            </Collapsible>
         </div>
     )
 }
