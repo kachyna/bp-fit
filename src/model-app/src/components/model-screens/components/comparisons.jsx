28473e6655b07ac2df7146374dfe29e9279bd854
René Kachyňa <kachynarene@gmail.com>
2026-05-22 22:55:27 +0200

    feat: create a component for esg cards

---
+++ src/model-app/src/components/model-screens/components/comparisons.jsx
@@ -0,0 +1,22 @@
+import { HelpCircle } from "lucide-react"
+
+export const ComparisonData = ({ sources, className, children }) => (
+    <div className="flex justify-left gap-2 items-center">
+        <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1">
+                <a href="#"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        sources.forEach(source => window.open(source, "_blank"));
+                    }}
+                    className={className}
+                    title="Zdroje dat">
+                    <div className="flex flex-row gap-1 items-center">
+                        <HelpCircle className="h-3 w-3" />
+                    </div>
+                </a>
+            </div>
+            {children}
+        </div>
+    </div>
+)
