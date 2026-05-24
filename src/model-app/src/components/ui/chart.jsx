6f0b899691334eba8e92ffe769ea4ebe6d07d45e
René Kachyňa <kachynarene@gmail.com>
2026-05-24 21:50:53 +0200

    feat: change chart component locale and add the possiblity to add units

--- src/model-app/src/components/ui/chart.jsx
+++ src/model-app/src/components/ui/chart.jsx
@@ -73,13 +73,13 @@ const ChartStyle = ({
           .map(([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
 ${colorConfig
-.map(([key, itemConfig]) => {
-const color =
-  itemConfig.theme?.[theme] ??
-  itemConfig.color
-return color ? `  --color-${key}: ${color};` : null
-})
-.join("\n")}
+              .map(([key, itemConfig]) => {
+                const color =
+                  itemConfig.theme?.[theme] ??
+                  itemConfig.color
+                return color ? `  --color-${key}: ${color};` : null
+              })
+              .join("\n")}
 }
 `)
           .join("\n"),
@@ -102,7 +102,8 @@ function ChartTooltipContent({
   formatter,
   color,
   nameKey,
-  labelKey
+  labelKey,
+  unit = ""
 }) {
   const { config } = useChart()
 
@@ -208,7 +209,7 @@ function ChartTooltipContent({
                       {item.value != null && (
                         <span className="font-mono font-medium text-foreground tabular-nums">
                           {typeof item.value === "number"
-                            ? item.value.toLocaleString()
+                            ? `${item.value.toLocaleString("cs-CZ")} ${unit}`
                             : String(item.value)}
                         </span>
                       )}
@@ -285,8 +286,8 @@ function getPayloadConfigFromPayload(
 
   const payloadPayload =
     "payload" in payload &&
-    typeof payload.payload === "object" &&
-    payload.payload !== null
+      typeof payload.payload === "object" &&
+      payload.payload !== null
       ? payload.payload
       : undefined
 
