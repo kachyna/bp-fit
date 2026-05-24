57dd8fb61bd72cab647d658a3143264f9e831510
René Kachyňa <kachynarene@gmail.com>
2026-05-24 22:32:53 +0200

    feat: set default leading

--- src/model-app/src/logic/utility.js
+++ src/model-app/src/logic/utility.js
@@ -1,3 +1,3 @@
-export const toThousands = (val, leading) => Number(((val || 0) / 1000).toFixed(leading))
-export const toMillions = (val, leading) => Number(((val || 0) / 1000000).toFixed(leading))
-export const toBillions = (val, leading) => Number(((val || 0) / 1000000000).toFixed(leading))
+export const toThousands = (val, leading = 3) => Number(((val || 0) / 1000).toFixed(leading))
+export const toMillions = (val, leading = 3) => Number(((val || 0) / 1000000).toFixed(leading))
+export const toBillions = (val, leading = 3) => Number(((val || 0) / 1000000000).toFixed(leading))
