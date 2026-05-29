7ec74a499978d28bb3eec4b26c06cdd82a2428c2
René Kachyňa <kachynarene@gmail.com>
2026-05-29 12:45:35 +0200

    docs: add documentation comments

--- src/model-app/src/components/model-screens/texts/textutils.js
+++ src/model-app/src/components/model-screens/texts/textutils.js
@@ -1,8 +1,32 @@
 import { CALCULATION_DETAILS } from "@/constants/calculation-details"
 
-const formatExp = (val, leading, exp = 0) =>
+/**
+ * Formats a number with leading zeros and scientific notation.
+ * Automatically converts to Czech locale numbers.
+ * 
+ * @param {number} val The value to format.
+ * @param {number} leading The number of leading zeros to add.
+ * @param {number} exp The exponent to use for scientific notation.
+ * @returns {string} The formatted number.
+ */
+const formatExp = (val, leading = 3, exp = 0) =>
     `${(val / 10 ** exp).toLocaleString("cs-CZ", { minimumFractionDigits: leading, maximumFractionDigits: leading })}`
 
+/**
+ * Formats a number with optional leading zeros and scientific notation based on the unit: 
+ * 
+ * - If the unit starts with "tis.", the number is divided by 1000.
+ * - If the unit starts with "mil.", the number is divided by 1 000 000.
+ * - If the unit starts with "mld.", the number is divided by 1 000 000 000.
+ * - Otherwise the number is not changed.
+ * 
+ * Automatically converts to Czech locale.
+ * 
+ * @param {number} val The value to format.
+ * @param {number} leading The number of leading zeros to add. Default is 3.
+ * @param {string} unit The unit to append. 
+ * @returns {string} The formatted number with the unit.
+ */
 export const formatData = (val, leading = 3, unit = "") => {
     if (unit.startsWith("tis.")) {
         return formatExp(val, leading, 3) + " " + unit
@@ -16,9 +40,15 @@ export const formatData = (val, leading = 3, unit = "") => {
     return formatExp(val, leading, 0) + " " + unit
 }
 
-
-// Returns an object of keys with formatted and raw values for easier management
-// Keys are taken from the input array, data from data object and format from CALCULATION_DETAILS
+/**
+ * Prepares data for display by formatting the values and adding them to an object.
+ * This function uses CALCULATION_DETAILS located in constants folder for formatting parameters. 
+ * If the key is not found in CALCULATION_DETAILS, it will throw an error.
+ * 
+ * @param {object} data The data object to prepare.
+ * @param {string[]} keys The keys to use for formatting.
+ * @returns {object} An object of keys with formatted and raw values for easier management.
+ */
 export const prepareData = (data, keys) => {
     const ret = {}
     keys.forEach(key => {
