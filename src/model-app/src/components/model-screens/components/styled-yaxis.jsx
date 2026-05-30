72ccafa08013187f9d7954e20b3b6e050849dc67
René Kachyňa <kachynarene@gmail.com>
2026-05-30 10:13:54 +0200

    fix: hide chart axis on mobile

--- src/model-app/src/components/model-screens/components/styled-yaxis.jsx
+++ src/model-app/src/components/model-screens/components/styled-yaxis.jsx
@@ -1,18 +1,23 @@
 import { YAxis } from "recharts"
+import { useIsMobile } from "#hooks/use-mobile"
 
 export const StyledYAxis = ({
   tickLine = false,
   axisLine = false,
   tick = { fill: "#64748b", fontSize: 10 },
-  width = 100,
+  width = 80,
+  hide,
   ...props
 }) => {
+  const isMobile = useIsMobile()
+
   return (
     <YAxis
       tickLine={tickLine}
       axisLine={axisLine}
       tick={tick}
       width={width}
+      hide={hide !== undefined ? hide : isMobile}
       {...props}
     />
   )
