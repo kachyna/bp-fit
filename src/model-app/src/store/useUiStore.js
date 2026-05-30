998a34043262292fa7155a05dc0f06bc3fe6abaf
René Kachyňa <kachynarene@gmail.com>
2026-05-30 09:19:06 +0200

    feat: default expand all cards if on mobile

--- src/model-app/src/store/useUiStore.js
+++ src/model-app/src/store/useUiStore.js
@@ -1,8 +1,10 @@
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 
+const isMobile = typeof window !== 'undefined' ? window.innerWidth < 768 : false
+
 export const useUiStore = create(immer((set) => ({
-    expandAllCards: false,
+    expandAllCards: isMobile,
     setExpandAllCards: (val) => set((state) => {
         state.expandAllCards = val
     }),
