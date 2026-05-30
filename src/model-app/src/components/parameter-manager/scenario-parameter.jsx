e479e4e8629b57cb6b0ff8147228c54ee653adbc
René Kachyňa <kachynarene@gmail.com>
2026-05-30 09:15:06 +0200

    feat: update parameter settings for mobile

--- src/model-app/src/components/parameter-manager/scenario-parameter.jsx
+++ src/model-app/src/components/parameter-manager/scenario-parameter.jsx
@@ -1,30 +1,40 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { InfoLabel } from "@/components/ui/info-label"
 import { BufferedInput } from "@/components/ui/buffered-input"
+import { SCENARIO_LABELS } from "@/constants/config"
+import { useIsMobile } from "#hooks/use-mobile"
 
-export const ScenarioParameterInput = ({ paramKey, label, description, paramValues, onChange }) => (
-    <Card className="shadow-sm border-slate-200">
-        <CardContent>
-            <div className="space-y-1.5">
-                <InfoLabel label={label} description={description} />
+export const ScenarioParameterInput = ({ paramKey, label, description, paramValues, onChange }) => {
+    const isMobile = useIsMobile()
 
-                <div className="flex flex-row gap-2">
-                    {Object.entries(paramValues).map(([scenario, value]) => (
-                        <BufferedInput key={scenario} className="h-8 text-sm [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                            id={`${scenario}-${paramKey}`}
-                            type="number"
-                            value={value}
-                            onChange={(e) => onChange(e, scenario)}
-                            placeholder="0"
-                            validate={(value) => {
-                                if (value < 0) return 0;
-                                return value;
-                            }}
-                            error="Parametr musí být vyšší nebo roven 0."
-                        />
-                    ))}
+    return (
+
+        <Card className="shadow-sm border-slate-200">
+            <CardContent>
+                <div className="space-y-1.5">
+                    <InfoLabel label={label} description={description} />
+
+                    <div className="flex flex-col sm:flex-row gap-2">
+                        {Object.entries(paramValues).map(([scenario, value]) => (
+                            <div key={scenario} className={isMobile ? "grid grid-cols-5 items-center" : ""}>
+                                {isMobile && <p className="text-xs text-muted-foreground col-span-2">{SCENARIO_LABELS[scenario]}:</p>}
+                                <BufferedInput className="col-span-3 h-8 text-sm [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                                    id={`${scenario}-${paramKey}`}
+                                    type="number"
+                                    value={value}
+                                    onChange={(e) => onChange(e, scenario)}
+                                    placeholder="0"
+                                    validate={(value) => {
+                                        if (value < 0) return 0;
+                                        return value;
+                                    }}
+                                    error="Parametr musí být vyšší nebo roven 0."
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </CardContent>
-    </Card>
-)
+            </CardContent>
+        </Card >
+    )
+}
